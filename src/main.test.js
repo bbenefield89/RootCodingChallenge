4d331461c92b7a3db24f177069c7456aebf62895
bbenefield89 <bsquared18@gmail.com>
2018-12-02 13:23:15 -0500

    REFACTOR ENTRY POINT OF APPLICATION AND TESTS
    - src/main.js
    - src/main.test.js

--- src/main.test.js
+++ src/main.test.js
@@ -1,3 +1,36 @@
-test('Hello, World!', () => {
-  expect('Hello, World').toEqual('Hello, World')
+import { createDriversRecordsHash } from './main'
+
+describe('Main.js', () => {
+  test('createDriversRecordsHash', () => {
+    let driverRecordsData = 'Driver Dan\n'
+    driverRecordsData += 'Driver Alex\n'
+    driverRecordsData += 'Driver Bob\n'
+    driverRecordsData += 'Trip Dan 07:15 07:45 17.3\n'
+    driverRecordsData += 'Trip Dan 06:12 06:32 21.8\n'
+    driverRecordsData += 'Trip Alex 12:01 13:16 42.0'
+
+    const hardCodedDriverRecordHash = {
+      "Alex": {
+          "__driverName": "Alex",
+          "__milesPerHour": 0,
+          "__totalDrivingDistInMiles": 42,
+          "__totalDrivingTimeInHrs": 1.25,
+        },
+        "Bob": {
+          "__driverName": "Bob",
+          "__milesPerHour": 0,
+          "__totalDrivingDistInMiles": 0,
+          "__totalDrivingTimeInHrs": 0,
+        },
+        "Dan": {
+          "__driverName": "Dan",
+          "__milesPerHour": 0,
+          "__totalDrivingDistInMiles": 39,
+          "__totalDrivingTimeInHrs": 0.833333333333333,
+        },
+      }
+    
+    const driverRecordHash = createDriversRecordsHash(driverRecordsData)
+    expect(driverRecordHash).toEqual(hardCodedDriverRecordHash)
+  })
 })
